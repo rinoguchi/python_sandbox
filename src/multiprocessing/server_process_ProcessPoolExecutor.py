a326fc17f9c303d43ce84ad47127577682c01ddf
rinoguchi <inogu1979@gmail.com>
2020-07-23 18:24:24 +0900

    シンプルなサーバプロセスの例を追加

---
+++ src/multiprocessing/server_process_ProcessPoolExecutor.py
@@ -0,0 +1,48 @@
+"""
+サーバプロセス × ProcessPoolExecutor
+"""
+from multiprocessing import Manager, Value
+from concurrent import futures
+from concurrent.futures import Future, ProcessPoolExecutor
+from typing import List
+import dataclasses
+
+MAX_WORKERS = 3
+
+
+@dataclasses.dataclass
+class SquaredNum:
+    num: int
+    squared_num: int
+
+
+def parallel_func(num: int, squared_nums: List[SquaredNum], squared_nums_total: Value):
+    squared_num: int = num * num
+    squared_nums.append(SquaredNum(num, squared_num))
+    squared_nums_total.value += squared_num
+
+
+def main():
+    nums = [0, 1, 2, 3, 4, 5]
+
+    with Manager() as manager:
+        squared_nums: List[SquaredNum] = manager.list()
+        squared_nums_total: Value = manager.Value('i', 0)
+        future_list: List[Future] = []
+        with ProcessPoolExecutor(max_workers=MAX_WORKERS) as executor:
+            for num in nums:
+                future: Future = executor.submit(
+                    parallel_func,
+                    num=num,
+                    squared_nums=squared_nums,
+                    squared_nums_total=squared_nums_total,
+                )
+                future_list.append(future)
+            for future in futures.as_completed(fs=future_list):
+                future.result()
+        print(f'squared_nums: {squared_nums}')
+        print(f'squared_nums_total: {squared_nums_total.value}')
+
+
+if __name__ == "__main__":
+    main()
