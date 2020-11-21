edf214228afef4f7fbcfbabee0234f2cc0df095f
rinoguchi <inogu1979@gmail.com>
2020-11-21 16:26:56 +0900

    AsyncGeneratorのyieldの例を改善

--- src/generator/async_generator_yield.py
+++ src/generator/async_generator_yield.py
@@ -4,26 +4,31 @@ from typing import AsyncGenerator
 
 
 async def main():
-    # async for i in async_generate():
-    #     print(f'get: {i}')
-    #     await asyncio.sleep(1)
+    print('====== async forの例 ======')
+    async for i in async_generate():
+        print(f'started loop. i: {i}')
+        await asyncio.sleep(1)
 
+    print('====== generatorを取得してwhileで回す例 ======')
     gen: AsyncGenerator[int, None] = async_generate()
+    i: int = 0
     while True:
         try:
-            i: int = await gen.__anext__()
-            print(f'get: {i}')
+            print(f'before anext. i: {i}')
+            i = await gen.__anext__()
+            print(f'after anext. i: {i}')
         except StopAsyncIteration:
             break
 
 
 async def async_generate() -> AsyncGenerator[int, None]:
-    print('started')
+    print('async_generate started.')
     for i in range(3):
+        print(f'before yield. i: {i}')
         yield i
+        print(f'after yield. i: {i}')
         await asyncio.sleep(1)
-        print(f'yielded: {i}')
-    print('finished')
+    print('async_generate finished.')
 
 
 if __name__ == "__main__":
