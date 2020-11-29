dd10d58f7f6792569c8e4978fe4bf49a7ab03b41
rinoguchi <inogu1979@gmail.com>
2020-11-29 11:01:00 +0900

    unittestのシンプルなケースを追加

---
+++ src/unittest/test_simple.py
@@ -0,0 +1,39 @@
+import unittest
+
+
+class TestSimple(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        """各クラスが実行される直前に一度だけ呼び出される"""
+        print('setUpClass called.')
+
+    @classmethod
+    def tearDownClass(cls):
+        """各クラスが実行された直後に一度だけ呼び出される"""
+        print('tearDownClass called.')
+
+    def setUp(self):
+        """各テストメソッドが実行される直前に呼び出される"""
+        print('setUp called.')
+
+    def tearDown(self):
+        """各テストメソッドが実行された直後に呼び出される"""
+        print('tearDown called.')
+
+    def test_lower(self):
+        print(f'test_lower called. self_id: {id(self)}')
+        self.assertEqual('HOGE'.lower(), 'hoge')
+
+    def test_upper(self):
+        print(f'test_upper called. self_id: {id(self)}')
+        self.assertEqual('hoge'.upper(), 'HOGE')
+
+    @unittest.skip("this test method will be skipped")
+    def test_split(self):
+        print(f'test_split called. self_id: {id(self)}')
+        self.assertEqual(len('aa bb'.split()), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
